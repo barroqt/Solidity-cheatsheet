823ad56226ed2e070e66a5328ebbf4bf90ff2fe6
barroqt <barroqt@gmail.com>
2022-03-03 17:01:51 +0100

    connection to lottery contract with web3

---
+++ lottery/front/src/lottery.js
@@ -0,0 +1,8 @@
+import web3 from "./web3";
+
+// run `node deploy.js` in the lottery folder and print the contract address here
+const address = 'AAAABBBBBBBCCCCCC';
+// same for abi
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+export default new web3.eth.Contract(abi, address);
